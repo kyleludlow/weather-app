5011414ffd0475bd0de97c41dd1bfd2981a1b169
jon <jon@idriveyourcar.com>
2016-03-30 19:42:33 +0200

    reverse geocode to get the city and call the api (with the same function that uses the html button)

--- www/controllers/searchWeatherByCity.js
+++ www/controllers/searchWeatherByCity.js
@@ -3,20 +3,17 @@ angular
 
   .controller('searchWeatherByCity',
 
-    function ($scope, dataService, $http) {
+    function ($scope, dataService) {
       $scope.weather = '';
       $scope.windSpeed = '';
       $scope.city = '';
       $scope.weatherIcon = '';
 
       $scope.searchWeather = function(city) {
-        console.log();
         dataService.apiWeather(city).then(function (result) {
           if(result.data.cod == 200) {
-            console.log(result);
             $scope.weather = result.data.list[0].weather[0].description;
             $scope.weatherIcon = 'http://openweathermap.org/img/w/'+ result.data.list[0].weather[0].icon + '.png';
-            console.log($scope.weatherIcon);
             $scope.windSpeed = result.data.list[0].wind.speed;
           } else {
             alert(result.data.message);
@@ -24,30 +21,38 @@ angular
         });
       };
 
-      var onSuccess = function(position) {
-        console.log(position);
-        var lat = Math.floor(position.coords.latitude);
-        var long = Math.floor(position.coords.longitude);
+      navigator.geolocation.getCurrentPosition(
+        function(pos) {
+          var geocoder = new google.maps.Geocoder();
+          var lat = pos.coords.latitude;
+          var lng = pos.coords.longitude;
+          var latlng = new google.maps.LatLng(lat, lng);
 
-        //alert('Latitude: '          + position.coords.latitude          + '\n' +
-        //  'Longitude: '         + position.coords.longitude         + '\n' +
-        //  'Altitude: '          + position.coords.altitude          + '\n' +
-        //  'Accuracy: '          + position.coords.accuracy          + '\n' +
-        //  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-        //  'Heading: '           + position.coords.heading           + '\n' +
-        //  'Speed: '             + position.coords.speed             + '\n' +
-        //  'Timestamp: '         + position.timestamp                + '\n');
-        return $http.get('http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=e9d20812185bcb41aa65bb443b85ad5f');
+          //reverse geocode the coordinates, returning location information.
+          geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+            var result = results[0];
+            var city = '';
+            var individualComponent = '';
 
-      };
+            for (var i = 0; i < result.address_components.length; i++) {
+              individualComponent = result.address_components[i];
 
-// onError Callback receives a PositionError object
-//
-      function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
-      }
+              if (individualComponent.types.indexOf("locality") >= 0) {
+                city = individualComponent.short_name;
+                break;
+              }
+            }
 
-      navigator.geolocation.getCurrentPosition(onSuccess, onError);
+            $scope.searchWeather(city);
+          });
+        },
+        function(err) {
+          alert("Something happened " + err);
+        });
     }
   );
+
+
+navigator.geolocation.getCurrentPosition(function (pos) {
+
+});
