ffcb366b2ce083c2fcc82e1acc56a0d25a1b04aa
Kyle <kyleludlow@gmail.com>
2016-03-30 09:56:56 -0700

    Added geolocation call, background image

--- www/controllers/searchWeatherByCity.js
+++ www/controllers/searchWeatherByCity.js
@@ -3,11 +3,10 @@ angular
 
   .controller('searchWeatherByCity',
 
-    function ($scope, dataService) {
+    function ($scope, dataService, $http) {
       $scope.weather = '';
       $scope.windSpeed = '';
       $scope.city = '';
-      $scope.name = 'name';
       $scope.weatherIcon = '';
 
       $scope.searchWeather = function(city) {
@@ -23,5 +22,32 @@ angular
             alert(result.data.message);
           }
         });
+      };
+
+      var onSuccess = function(position) {
+        console.log(position);
+        var lat = Math.floor(position.coords.latitude);
+        var long = Math.floor(position.coords.longitude);
+
+        //alert('Latitude: '          + position.coords.latitude          + '\n' +
+        //  'Longitude: '         + position.coords.longitude         + '\n' +
+        //  'Altitude: '          + position.coords.altitude          + '\n' +
+        //  'Accuracy: '          + position.coords.accuracy          + '\n' +
+        //  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+        //  'Heading: '           + position.coords.heading           + '\n' +
+        //  'Speed: '             + position.coords.speed             + '\n' +
+        //  'Timestamp: '         + position.timestamp                + '\n');
+        return $http.get('http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=e9d20812185bcb41aa65bb443b85ad5f');
+
+      };
+
+// onError Callback receives a PositionError object
+//
+      function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
       }
-    });
+
+      navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
+  );
--- www/services/dataService.js
+++ www/services/dataService.js
@@ -5,7 +5,6 @@ angular
 
     service.apiWeather = function(city) {
       console.log(city);
-      //return $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=e9d20812185bcb41aa65bb443b85ad5f');
       return $http.get('http://api.openweathermap.org/data/2.5/forecast/city?q=' + city + '&APPID=e9d20812185bcb41aa65bb443b85ad5f');
     };
 
