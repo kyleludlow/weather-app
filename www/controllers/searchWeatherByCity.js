14ef825302029c0fff74e174fd983f5a9af523c0
Kyle <kyleludlow@gmail.com>
2016-04-08 08:27:34 -0700

    Updated backgrounds and icon functionality

--- www/controllers/searchWeatherByCity.js
+++ www/controllers/searchWeatherByCity.js
@@ -8,13 +8,58 @@ angular
       $scope.windSpeed = '';
       $scope.city = '';
       $scope.weatherIcon = '';
+      $scope.iconColor = 'white-icons';
+      $scope.temperature = '';
+      $scope.humidity = '';
+      $scope.cityName = '';
+
+
+      var backgroundImage = '';
+      var textColor = 'white';
+      var svgColor = 'white';
+
+      var d = new Date();
+      var n = d.getHours();
+      switch (true) {
+        case (n < 7):
+          backgroundImage = 'url(../img/backgrounds/night.jpg)';
+          break;
+        case (n >= 7 && n < 12):
+          backgroundImage = 'url(../img/backgrounds/morning.jpg)';
+          break;
+        case (n >= 12 && n < 20):
+          backgroundImage = 'url(../img/backgrounds/sunny.jpg)';
+          textColor = 'black';
+          $scope.iconColor = 'black';
+          break;
+        case (n >= 20):
+          backgroundImage = 'url(../img/backgrounds/night.jpg)';
+          break;
+        default:
+          alert("Error");
+          break;
+      }
+
+
+      $('#home-view-body').css('backgroundImage', backgroundImage);
+      $('h2, h3').css('color', textColor);
+
+
+
+
+
+
 
       $scope.searchWeather = function(city) {
         dataService.apiWeather(city).then(function (result) {
           if(result.data.cod == 200) {
+            console.log(result);
             $scope.weather = result.data.list[0].weather[0].description;
-            $scope.weatherIcon = 'http://openweathermap.org/img/w/'+ result.data.list[0].weather[0].icon + '.png';
+            $scope.weatherIcon = result.data.list[0].weather[0].icon + '.png';
             $scope.windSpeed = result.data.list[0].wind.speed;
+            $scope.temperature = result.data.list[0].main.temp - 273.15;
+            $scope.humidity = result.data.list[0].main.humidity;
+            $scope.cityName = result.data.city.name;
           } else {
             alert(result.data.message);
           }
